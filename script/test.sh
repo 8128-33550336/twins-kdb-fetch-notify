38b60e1bf136dfb64a529edcdf866f52c57b8167
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-12-26 01:37:06 +0900

    up

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,4 @@
 npm run build
-cat ./sample/sample.http ./sample/diff.json | nc -l 9090 &
-echo | nc -l 9091 | sed '1,/^\r$/d' | jq '.' &
+cat ./sample/sample.http ./sample/diff.json | nc -l 0.0.0.0 9090 &
+echo | nc -l 0.0.0.0 9091 | sed '1,/^\r$/d' | jq '.' &
 npm run start
