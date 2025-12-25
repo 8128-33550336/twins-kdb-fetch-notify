214bf1d7d83da70aa8175be1e83e419834b2d86d
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-12-25 23:00:38 +0900

    clean

--- src/types.ts
+++ src/types.ts
@@ -19,29 +19,6 @@ export type InstructionalType = {
     readonly flags: InstructionalTypeFlags;
 };
 
-const instructionalTypeMap: Record<InstructionalTypeCode, InstructionalType> = {
-    "0": { text: "その他", flags: { 講義: false, 演習: false, "実習･実験･実技": false, "卒業論文･卒業研究等": false, その他: true } },
-    "1": { text: "講義", flags: { 講義: true, 演習: false, "実習･実験･実技": false, "卒業論文･卒業研究等": false, その他: false } },
-    "2": { text: "演習", flags: { 講義: false, 演習: true, "実習･実験･実技": false, "卒業論文･卒業研究等": false, その他: false } },
-    "3": { text: "実習･実験･実技", flags: { 講義: false, 演習: false, "実習･実験･実技": true, "卒業論文･卒業研究等": false, その他: false } },
-    "4": { text: "講義及び演習", flags: { 講義: true, 演習: true, "実習･実験･実技": false, "卒業論文･卒業研究等": false, その他: false } },
-    "5": { text: "講義及び実習･実験･実技", flags: { 講義: true, 演習: false, "実習･実験･実技": true, "卒業論文･卒業研究等": false, その他: false } },
-    "6": { text: "演習及び実習･実験･実技", flags: { 講義: false, 演習: true, "実習･実験･実技": true, "卒業論文･卒業研究等": false, その他: false } },
-    "7": { text: "講義、演習及び実習･実験･実技", flags: { 講義: true, 演習: true, "実習･実験･実技": true, "卒業論文･卒業研究等": false, その他: false } },
-    "8": { text: "卒業論文･卒業研究等", flags: { 講義: false, 演習: false, "実習･実験･実技": false, "卒業論文･卒業研究等": true, その他: false } },
-};
-
-function isInstructionalTypeCode(code: string): code is InstructionalTypeCode {
-    return code in instructionalTypeMap;
-}
-
-export const getInstructionalType = (code: string): InstructionalType => {
-    if (!isInstructionalTypeCode(code)) {
-        throw new Error(`Unknown instructional type code: ${code}`);
-    }
-    return instructionalTypeMap[code];
-};
-
 export type Terms = { text: "春学期"; code: "A" } | { text: "秋学期"; code: "B" };
 
 export type DaysOfWeek = "月" | "火" | "水" | "木" | "金" | "土" | "日" | "他";
