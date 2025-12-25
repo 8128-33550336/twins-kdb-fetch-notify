76bcee4bded63c99e7be7fcc1a8b05f07a10ba5a
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-12-25 23:08:49 +0900

    エラーにする

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,4 @@ const main = async (): Promise<void> => {
     console.log(`差分件数: ${Object.keys(diff).length}`);
 };
 
-main().catch((error: unknown) => {
-    if (error instanceof Error) {
-        console.error(`実行エラー: ${error.message}`);
-        return;
-    }
-
-    console.error("不明なエラーが発生しました。", error);
-});
+main()
